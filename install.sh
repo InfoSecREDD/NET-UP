e550338ef2da52ea2031c19cef8024e54d3e5792
PrivateLocker <40194049+PrivateLocker@users.noreply.github.com>
2018-10-20 17:38:16 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,31 @@
 #!/usr/bin/env bash
+root_check_init(){
+    echo "Checking for Root.."
+    if [[ $EUID -ne 0 ]]; then
+        echo "This script must be ran with sudo or root privileges, or this isn't going to work."
+            exit 1
+    else
+        echo "Root Access Granted!"
+    fi
+}
+root_check_init;
 HOMEDIR=$HOME/netup
+if [[ -d "$HOMEDIR" ]] ; then
+    echo "Directory already exists..";
+    echo "Removing old copy..";
+    rm -rf $HOMEDIR
+else
+    echo "$HOMEDIR already exists.."
+fi
 cd $HOME
 echo "Cloning from GitHub..";
 git clone https://github.com/private-locker/Net-UP.git netup
 cd $HOMEDIR
 chmod +x $HOMEDIR/netup
+chmod +x $HOMEDIR/modules/enablemod
+chmod +x $HOMEDIR/modules/disablemod
 source $HOMEDIR/netup
+rm -rf "$0"
+exit
 
 
