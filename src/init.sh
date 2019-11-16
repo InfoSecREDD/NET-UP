a04db36f62b7108bc1d3694cfbde1e3607f6de0e
Private-Locker, LLC <40194049+PrivateLocker@users.noreply.github.com>
2019-11-15 16:36:39 -0800

    Update init.sh

--- src/init.sh
+++ src/init.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # Script-Wide Variables/Functions
-
+NETUPLOGFILE=$DIR/netup.log
+MODULELOGFILE=$DIR/netup-modules.log
+LOGDATE=$(date '+%Y-%m-%d %H:%M:%S')
 chklog_status(){
     if [[ $LOGVERBOSE != NULL ]]; then
-	NETUPLOGFILE=$DIR/netup.log
-	MODULELOGFILE=$DIR/netup-modules.log
-	LOGDATE=$(date '+%Y-%m-%d %H:%M:%S')
 	if [[ ! -f "$NETUPLOGFILE" ]]; then
 	     touch "$NETUPLOGFILE";
 	fi
