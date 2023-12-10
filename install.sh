eb4124b268f245b2f245a21a08a055fa47f56777
REDD <ar1k88gamingllc@gmail.com>
2023-12-09 21:06:49 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,18 +14,18 @@ DELFILE="$DIR/$0"
 root_check_init(){
     echo "Checking for Root.."
     if [[ $EUID -ne 0 ]]; then
-        echo "This script must be ran with sudo or root privileges, or this isn't going to work."
+        echo -e "This script must be ran with sudo or root privileges, or this isn't going to work."
             exit 1
     else
-        echo "Root Access Granted!"
+        echo -e "Root Access Granted!"
     fi
 }
 clonemain(){
-        echo "Cloning from GitHub..";
+        echo -e "Cloning from GitHub..";
         git clone https://github.com/private-locker/NET-UP.git $HOMEDIR
 }
 clonemods(){
-        echo "Cloning Modules from Repo.";
+        echo -e "Cloning Modules from Repo.";
         git clone https://github.com/private-locker/modules.git $HOMEDIR/modules
 }
 changeperm(){
@@ -49,8 +49,8 @@ run(){
 }
 root_check_init;
 if [[ -d "$HOMEDIR" ]] ; then
-    echo "Directory already exists..";
-    echo "Removing old copy..";
+    echo -e "Directory already exists..";
+    echo -e "Removing old copy..";
     rm -rf $HOMEDIR
 else
     echo "$HOMEDIR does not exists.."
@@ -76,6 +76,27 @@ fi
 changeperm;
 cleanup;
 run;
+HOMEPATH="$HOME/netup"
+HOMEDIR="/usr/local/bin/netup"
+
+if [ -L "$HOMEDIR" ]; then
+    rm "$HOMEDIR"
+    if [ $? -eq 0 ]; then
+        echo -e "Existing symlink deleted successfully."
+    else
+        echo -e "Error deleting existing symlink."
+        exit 1
+    fi
+fi
+
+ln -s "$HOMEPATH" "$HOMEDIR"
+
+if [ $? -eq 0 ]; then
+    echo -e "New symlink created successfully."
+else
+    echo -e "Error creating symlink."
+fi
+
 echo -e "Exiting.. You can now use netup anywhere in the system."
 # Double checking if file exists, if so delete. Precautionary measure.
 if [ -f "$DELFILE" ]; then
